49bd998322f6a8c9d66bff88ac423953927f61ca
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-28 04:57:40 +0200

    setup MapFragment

--- app/src/main/java/com/example/mapboxnavigationintegration/MainActivity.kt
+++ app/src/main/java/com/example/mapboxnavigationintegration/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.mapboxnavigationintegration
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.mapboxnavigationintegration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fragmentMap: MapFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initObjects()
+        initSubView()
+    }
+
+    private fun initObjects() {
+        fragmentMap = MapFragment()
+    }
+
+    private fun initSubView() {
+        addFragment(fragmentMap)
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_map, fragment)
+        transaction.commit()
+    }
+
+    private fun removeFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.remove(fragment)
+        transaction.commit()
     }
 }
