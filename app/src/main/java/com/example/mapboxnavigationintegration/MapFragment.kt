c5e8375c2f67b0a5b6eb92d14b434f208e6773ca
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-28 06:11:48 +0200

    add map view

--- app/src/main/java/com/example/mapboxnavigationintegration/MapFragment.kt
+++ app/src/main/java/com/example/mapboxnavigationintegration/MapFragment.kt
@@ -16,7 +16,7 @@ class MapFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMapBinding.inflate(inflater, container, false)
         return binding.root
     }
